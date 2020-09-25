d238ff6ff80c9a628d07525e380aedd89bbafee6
QndziaPL <maksymilian.rogacewicz@gmail.com>
2020-09-25 17:45:07 +0200

    added fiberdirect plus some polishments v2

---
+++ examples/FiberForward.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once realpath(dirname(__FILE__)) . '../../lib/FiberPayClient.php';
+
+$apiKey = 'twój klucz publiczny';
+$apiSecret = 'twój klucz prywatny';
+
+$client = new \FiberPay\FiberPayClient($apiKey, $apiSecret, true);
+
+function fiberForward(){
+    global $client;
+
+    /** wymagane */
+    $targetName = "Zbigniew Sieczkowski";
+    $targetIban = 'PL27114020040000300201355387';
+    $brokerName = "Janina Mocarz";
+    $brokerIban = 'PL93234120040015780201352241';
+    $description = "Tutuł przelewu";
+    $sourceAmount = 13572.80;
+    $targetAmount = 10780.30;
+    $currency = 'PLN';  // na chwilę obecną jedyna dostępna opcja
+
+    /** opcjonalnie */
+    $callbackUrl = 'https://your.api/callback';
+    $callbackParams = 'callback params';
+    $metadata = 'dodatkowe informacje';
+
+    $response = $client->createForward($targetName, $targetIban,$brokerName,$brokerIban, $description, $sourceAmount, $targetAmount, $currency, "", "", "");
+    $responseOptional = $client->createForward($targetName, $targetIban,$brokerName, $brokerIban, $description, $sourceAmount, $targetAmount,
+        $currency, $callbackUrl, $callbackParams, $metadata);
+
+    echo "------    fiberForward     ------"."\n".$response;
+    echo "------    fiberForward     ------"."\n".$responseOptional;
+
+}
+
+fiberForward();

29003bc41e3301b672b0d47f681077c1514da48e
slawek <slawomir.wiech@inpay.pl>
2020-09-30 16:57:04 +0200

    fixed FiberDirect example

--- examples/FiberDirect.php
+++ examples/FiberDirect.php
@@ -8,38 +8,21 @@ $apiSecret = 'twój_klucz_tajny';
 
 $client = new \FiberPay\FiberPayClient($apiKey, $apiSecret, true);
 
-
-
-
 /** symuluje proces tworzenia przelewu */
-fiberDirect();
 
+$toName = "Zbigniew Sieczkowski";
+$toIban = 'PL27114020040000300201355387';
+$description = "Tutuł przelewu";
+$amount = 21056.30;
+$currency = 'PLN';  // na chwilę obecną jedyna dostępna opcja
 
-/***********************************************************
- *                                                         *
- *                      Funkcje                            *
- *                                                         *
- **********************************************************/
+/** opcjonalnie */
+$callbackUrl = 'https://your.api/callback';
+$callbackParams = 'callback params';
+$metadata = 'dodatkowe informacje';
 
+$response = $client->createDirect($toName, $toIban, $description, $amount, $currency);
+$responseOptional = $client->createDirect($toName, $toIban, $description, $amount, $currency, $callbackUrl, $callbackParams, $metadata);
 
-function fiberDirect(){
-    global $client;
-
-    /** wymagane */
-    $toName = "Zbigniew Sieczkowski";
-    $toIban = 'PL27114020040000300201355387';
-    $description = "Tutuł przelewu";
-    $amount = 21056.30;
-    $currency = 'PLN';  // na chwilę obecną jedyna dostępna opcja
-
-    /** opcjonalnie */
-    $callbackUrl = 'https://your.api/callback';
-    $callbackParams = 'callback params';
-    $metadata = 'dodatkowe informacje';
-
-    $response = $client->createDirect($toName, $toIban, $description, $amount, $currency);
-    $responseOptional = $client->createDirect($toName, $toIban, $description, $amount, $currency, $callbackUrl, $callbackParams, $metadata);
-
-    echo "------    fiberDirect     ------"."\n".$response;
-}
+echo "------    fiberDirect     ------\n" . $response;
 
